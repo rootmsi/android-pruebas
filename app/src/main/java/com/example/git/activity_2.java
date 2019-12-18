b66dcfad246c1863421e638b950db88268dd32a1
Manu <manubartman@gmail.com>
2019-12-18 16:19:21 +0100

    Añadir las activities

--- app/src/main/java/com/example/git/activity_2.java
+++ app/src/main/java/com/example/git/activity_2.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Activity2 extends AppCompatActivity {
+public class activity_2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
