54defa403d72611b7a3ba27c1f564282a40171a6
Manu <manubartman@gmail.com>
2019-12-18 20:51:51 +0100

    Commit manu menu superior 20:51

--- app/src/main/java/com/example/git/MainActivity.java
+++ app/src/main/java/com/example/git/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +28,22 @@ public class MainActivity extends AppCompatActivity {
     public void continuar(View view){
         Intent intent1 = new Intent(this, activity_2.class);
         startActivity(intent1);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_superior, menu);
+        return true;
+    }
+
+    public  boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        Intent intent;
+        if (id == R.id.item_cues) {
+            intent = new Intent(this, activity_6.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
