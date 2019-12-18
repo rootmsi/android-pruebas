e563ee2f157404428da1270291ec9ef85b01a2cc
Manu <manubartman@gmail.com>
2019-12-18 18:41:40 +0100

    Commit Paloma 18:41

--- app/src/main/java/com/example/git/activity_4.java
+++ app/src/main/java/com/example/git/activity_4.java
@@ -2,11 +2,14 @@ package com.example.git;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class activity_4 extends AppCompatActivity {
+public class activity_4 extends AppCompatActivity implements View.OnClickListener {
 
     EditText texto;
     Button boton;
@@ -19,5 +22,18 @@ public class activity_4 extends AppCompatActivity {
         texto=findViewById(R.id.editTextDenuncia);
         boton=findViewById(R.id.buttonSMS);
 
+        boton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setData(Uri.parse("smsto:"));  // This ensures only SMS apps respond
+        intent.putExtra("sms_body", "IGUALDAD");
+      //  intent.putExtra(Intent.EXTRA_STREAM, attachment);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
