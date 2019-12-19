557ee2b088429778913cdc6592ec98b842c57e67
Lionel <leonjuman@gmail.com>
2019-12-19 17:12:40 +0100

    commit A6 Layout y Codigo del A5 Lionel 17:12

--- app/src/main/java/com/example/git/activity_5.java
+++ app/src/main/java/com/example/git/activity_5.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -21,11 +23,35 @@ public class activity_5 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_5);
+        
         llamada=findViewById(R.id.imageView016);
         web=findViewById(R.id.imageViewGuardiaCivil);
         
         guardiaCivil=findViewById(R.id.textViewGC);
         info016=findViewById(R.id.textView016);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+    }
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_superior, menu);
+        return true;
+    }
+
+    public  boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        Intent intent;
+        if (id == R.id.item_cues) {
+            intent = new Intent(this, activity_6.class);
+            startActivity(intent);
+        }
+
+        if(id == R.id.item_acerca){
+            intent = new Intent(this, activity_8.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
     public  void llamada(View view){
         Intent llamada= new Intent(Intent.ACTION_DIAL);
